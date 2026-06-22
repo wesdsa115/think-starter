168f4b664b070c49d9d8fc3caccb0b8638599c15
Confidence Okoghenun <cokoghenun@gmail.com>
2026-06-22 04:17:43 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [react(), cloudflare(), tailwindcss()],
+	build: {
+	  commonjsOptions: {
+	    transformMixedEsModules: true,
+	  },
+	},
 });
